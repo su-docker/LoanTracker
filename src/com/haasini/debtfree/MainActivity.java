d56aef4f0f38141c794af9d02e19ce1d1b976e40
Sudhakar <sudhakar82@gmail.com>
2012-08-20 23:08:52 +0530

    Making this an android app

---
+++ src/com/haasini/debtfree/MainActivity.java
@@ -0,0 +1,21 @@
+package com.haasini.debtfree;
+
+import android.os.Bundle;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+		// setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
